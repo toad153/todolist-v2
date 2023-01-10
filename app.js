d480ecb18b40abf70bae5582eb477e9c3ef9542a
toad153 <76248516+toad153@users.noreply.github.com>
2023-01-10 20:09:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+//jshint eversion:6
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const { reset } = require("nodemon");
+const date = require(__dirname + "/date.js")
+
+// console.log(date());
+
+const app = express();
+
+const items = [];
+const workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    
+    const day = date.getDate();
+    res.render("list", { listTitle: day, newListItems: items});
+
+});
+
+
+app.post("/", function(req,res){
+
+    let item = req.body.newItem;
+
+    if (req.body.list === "Work List"){
+        workItems.push(item);
+        res.redirect("/work");
+    } else{
+        items.push(item);
+        res.redirect("/");
+    }
+
+});
+
+app.get("/work",function(req,res){
+    res.render("list", { listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about",function(req,res){
+    res.render("about");
+});
+
+// app.post("/work", function(req,res){
+//     let item = req.body.newItem;
+
+//     workItems.push(item);
+
+//     res.redirect("/work")
+// });
+
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000");
+});
---
+++ date.js
@@ -0,0 +1,31 @@
+//jshint eversion:6
+
+exports.getDate = function() {
+
+    const today = new Date();
+
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    return today.toLocaleDateString("en-US", options);
+}
+
+exports.getDay = function () {
+    const today = new Date();
+
+    const options = {
+        weekday: "long",
+    };
+
+    return today.toLocaleDateString("en-US", options);
+}
+
+console.log(module.exports);
+
+
+
+
+
